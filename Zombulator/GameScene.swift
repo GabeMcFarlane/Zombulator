6253403393182e2fec58fa10894783a69292966f
Gabe McFarlane <gabrielmcfarlane65@gmail.com>
2017-04-12 18:00:37 -0700

    Generate basic zombie and human sprite population with count.

--- Zombulator/GameScene.swift
+++ Zombulator/GameScene.swift
@@ -11,83 +11,78 @@ import GameplayKit
 
 class GameScene: SKScene {
     
+    let totalPopulation = 100
+    
     var entities = [GKEntity]()
     var graphs = [String : GKGraph]()
     
     private var lastUpdateTime : TimeInterval = 0
     private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    private var prototypeNode : SKShapeNode?
+    private var zombieCount = 0
+    private var humanCount = 0
     
     override func sceneDidLoad() {
 
         self.lastUpdateTime = 0
         
-        // Get label node from scene and store it for use later
+        // Create shape node to use during mouse interaction
+        let w = (self.size.width + self.size.height) * 0.008
+        self.prototypeNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+        
+//        if let prototypeNode = self.prototypeNode {
+//            //prototypeNode.lineWidth = 1
+//            
+//            //prototypeNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(M_PI), duration: 1)))
+////            prototypeNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
+////                                              SKAction.fadeOut(withDuration: 0.5),
+////                                              SKAction.removeFromParent()]))
+//        }
+        
+        for _ in 0..<totalPopulation {
+            let probability = arc4random_uniform(5)
+            if probability < 3 {
+                addZombieToScene()
+                zombieCount += 1
+            } else {
+                addHumanToScene()
+                humanCount += 1
+            }
+        }
+        
         self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
         if let label = self.label {
             label.alpha = 0.0
             label.run(SKAction.fadeIn(withDuration: 2.0))
+            label.text = "zombies: \(zombieCount) humans: \(humanCount)"
         }
-        
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(M_PI), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
+
+    }
+    
+    private func addZombieToScene() {
+        if let zombieNode = prototypeNode?.copy() as! SKShapeNode? {
+            zombieNode.position = CGPoint(x: randomX(), y: randomY(min: self.size.height * 0.25, max: self.size.height / 2))
+            zombieNode.strokeColor = SKColor.green
+            addChild(zombieNode)
         }
     }
     
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
+    private func addHumanToScene() {
+        if let humanNode = prototypeNode?.copy() as! SKShapeNode? {
+            humanNode.position = CGPoint(x: randomX(), y: randomY(min: self.size.height / -2, max: self.size.height * -0.25))
+            humanNode.strokeColor = SKColor.purple
+            addChild(humanNode)
         }
     }
+
     
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
+    private func randomX() -> CGFloat {
+        return self.size.width / -2 + CGFloat(arc4random_uniform(UInt32(self.size.width)))
     }
     
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
+    private func randomY(min: CGFloat, max: CGFloat) -> CGFloat {
+        return min + CGFloat(arc4random_uniform(UInt32(max - min)))
     }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
     
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
